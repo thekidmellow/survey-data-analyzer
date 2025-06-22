824e38f842ce5459a17441c137c2f8fd4e66dc58
thekidmellow <thekidmellow@gmail.com>
2025-06-22 21:23:12 +0200

    Initialize SurveyAnalyzer class with default configs

--- src/analyzer.py
+++ src/analyzer.py
@@ -3,3 +3,16 @@ from collections import Counter, defaultdict
 import statistics
 from colorama import Fore, Style
 import re
+
+
+class SurveyAnalyzer:
+    """
+    Comprehensive survey data analysis engine.
+    """
+    
+    def __init__(self):
+        """
+        Initialize the Survey Analyzer with analysis configurations.
+        """
+        self.analysis_results = {}
+        self.numeric_threshold = 0.8
