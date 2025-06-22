863ecd48df3eefccaab55df2dde7b9f5aed4be14
thekidmellow <thekidmellow@gmail.com>
2025-06-22 20:07:03 +0200

    Add DataManager class with constructor initializing supported formats and state

--- src/data_manager.py
+++ src/data_manager.py
@@ -9,3 +9,9 @@ import pandas as pd
 from pathlib import Path
 from typing import List, Dict, Any, Optional
 from colorama import Fore, Style
+
+class DataManager:
+    def __init__(self):
+        self.supported_formats = ['.csv', '.json', '.xlsx']
+        self.current_data = None
+        self.data_schema = None
