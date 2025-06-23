3598a8241745b4c82b7fecb47fd72bd0597e064b
thekidmellow <thekidmellow@gmail.com>
2025-06-23 16:59:46 +0200

    Add satisfaction analysis visualization

--- src/visualizer.py
+++ src/visualizer.py
@@ -26,3 +26,46 @@ class DataVisualizer:
             'warning': Fore.RED,
             'info': Fore.CYAN
         }
+
+    def create_comprehensive_report(self, analysis_results: Dict[str, Any]) -> str:
+        report_sections = []
+        
+        # Header
+        report_sections.append(self.create_report_header())
+        
+        # Basic Statistics Section
+        if 'basic_statistics' in analysis_results:
+            report_sections.append(self.visualize_basic_statistics(analysis_results['basic_statistics']))
+        
+        # Response Patterns Section
+        if 'response_patterns' in analysis_results:
+            report_sections.append(self.visualize_response_patterns(analysis_results['response_patterns']))
+        
+        # Satisfaction Analysis Section
+        if 'satisfaction_analysis' in analysis_results:
+            report_sections.append(self.visualize_satisfaction_analysis(analysis_results['satisfaction_analysis']))
+        
+        # Data Quality Section
+        if 'data_quality' in analysis_results:
+            report_sections.append(self.visualize_data_quality(analysis_results['data_quality']))
+        
+        # Footer
+        report_sections.append(self.create_report_footer())
+        
+        return "\n\n".join(report_sections)
+
+    def create_report_header(self) -> str:
+
+        header = f
+        
+        return header
+    
+    def get_current_date(self) -> str:
+        """
+        Get current date for report timestamp.
+        
+        Returns:
+            str: Current date string
+        """
+        from datetime import datetime
+        return datetime.now().strftime("%Y-%m-%d %H:%M:%S")    
