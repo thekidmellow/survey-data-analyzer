24fd862c99b05ea2f3d9628606dbda962fcb616d
thekidmellow <thekidmellow@gmail.com>
2025-06-26 22:31:18 +0200

    Fixing errors

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
+# setup.py
 from setuptools import setup, find_packages
 
 setup(
     name="survey_data_analyzer",
-    version="1.0.0",
+    version="0.1.0",
     packages=find_packages(),
-    install_requires=[
-        "Flask>=3.0.0",
-        "pandas>=2.0.0",
-        "matplotlib>=3.0.0"
-    ],
-    python_requires=">=3.8",
+    include_package_data=True,
+    install_requires=[],
 )
