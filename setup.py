07fc79eac354d55771f3e9430b733edb3adb0b2c
thekidmellow <thekidmellow@gmail.com>
2025-06-26 22:18:16 +0200

    Deleted runtime and added setup py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="survey_data_analyzer",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "Flask>=3.0.0",
+        "pandas>=2.0.0",
+        "matplotlib>=3.0.0",
+        "seaborn>=0.12.0"
+    ],
+    python_requires=">=3.12"
+)
