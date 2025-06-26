c8dc4a8977ce0101d63c7c3d2e44682a8b4e564d
thekidmellow <thekidmellow@gmail.com>
2025-06-26 16:47:46 +0200

    Fix app to use $PORT and update Procfile

--- web_app.py
+++ web_app.py
@@ -1,11 +1,10 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, Flask is running!"
+# your routes here
 
 if __name__ == "__main__":
-    app.run(port=5001, debug=True)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
