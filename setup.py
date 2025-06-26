82a792debf6c732dca663b230d39608c45f5482d
thekidmellow <thekidmellow@gmail.com>
2025-06-26 22:42:49 +0200

    Fix Python package structure with __init__.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-# setup.py
 from setuptools import setup, find_packages
 
 setup(
-    name="survey_data_analyzer",
-    version="0.1.0",
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[],
+    name='survey_data_analyzer',
+    version='0.1.0',
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[],  # or leave empty and use requirements.txt
+    entry_points={
+        "console_scripts": [
+            "analyze=main:main",  # if main.py has a main() function
+        ]
+    },
 )
+
